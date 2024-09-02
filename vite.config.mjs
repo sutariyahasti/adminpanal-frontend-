4662a4d8871c9d677317093e1d2c66d64527f5ce
sutariyahasti <hastisutariya01@gmail.com>
2024-09-02 11:00:34 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,3 @@
-// https://github.com/vitejs/vite/discussions/3448
-// import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
@@ -8,33 +6,24 @@ import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
-  // https://github.com/jpuri/react-draft-wysiwyg/issues/1317
-  base: 'https://adminpanal-frontend.vercel.app/free',
+  // Adjust the base path if necessary
+  base: process.env.VERCEL_ENV === 'production' ? '/free/' : '/',
   define: {
     global: 'window'
   },
   resolve: {
-    // alias: [
-    //   {
-    //     find: /^~(.+)/,
-    //     replacement: path.join(process.cwd(), 'node_modules/$1')
-    //   },
-    //   {
-    //     find: /^src(.+)/,
-    //     replacement: path.join(process.cwd(), 'src/$1')
-    //   }
-    // ]
+    // Custom path aliases if needed, otherwise can be removed
+    // alias: {
+    //   '@': '/src',
+    // }
   },
+  // The following server options are for local development and can be omitted for Vercel deployment
   server: {
-    // this ensures that the browser opens upon server start
     open: true,
-    // this sets a default port to 3000
-    port: 3000
+    port: 3000,
   },
   preview: {
-    // this ensures that the browser opens upon preview start
     open: true,
-    // this sets a default port to 3000
-    port: 3000
+    port: 3000,
   }
 });
